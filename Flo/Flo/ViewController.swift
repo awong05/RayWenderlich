c3baf58323b1f267c10a12dfe41b3a595ab12aa6
Andy Wong <awong@propelmarketing.com>
2016-08-25 00:17:24 -0400

    Part 2 of 3.

--- Flo/Flo/ViewController.swift
+++ Flo/Flo/ViewController.swift
@@ -13,15 +13,52 @@ class ViewController: UIViewController
     @IBOutlet weak var counterView: CounterView!
     @IBOutlet weak var counterLabel: UILabel!
 
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var graphView: GraphView!
+
+    @IBOutlet weak var averageWaterDrunk: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+
+    private var isGraphViewShowing = false
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
         counterLabel.text = "\(counterView.counter)"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupGraphDisplay()
+    {
+        let noOfDays = 7
+
+        graphView.graphPoints[graphView.graphPoints.count - 1] = counterView.counter
+        graphView.setNeedsDisplay()
+        maxLabel.text = "\(graphView.graphPoints.maxElement()!)"
+
+        let average = graphView.graphPoints.reduce(0, combine: +) / graphView.graphPoints.count
+        averageWaterDrunk.text = "\(average)"
+
+        let dateFormatter = NSDateFormatter()
+        let calendar = NSCalendar.currentCalendar()
+        let componentOptions: NSCalendarUnit = .Weekday
+        let components = calendar.components(componentOptions, fromDate: NSDate())
+
+        var weekday = components.weekday
+
+        let days = ["S", "S", "M", "T", "W", "T", "F"]
+
+        for i in (1...days.count).reverse() {
+            if let labelView = graphView.viewWithTag(i) as? UILabel {
+                if weekday == 7 {
+                    weekday = 0
+                }
+                labelView.text = days[weekday]
+                weekday -= 1
+                if weekday < 0 {
+                    weekday = days.count - 1
+                }
+            }
+        }
     }
 
     @IBAction func btnPushButton(button: PushButtonView)
@@ -34,5 +71,31 @@ class ViewController: UIViewController
             }
         }
         counterLabel.text = "\(counterView.counter)"
+
+        if isGraphViewShowing {
+            counterViewTap(nil)
+        }
+    }
+
+    @IBAction func counterViewTap(gesture: UITapGestureRecognizer?)
+    {
+        if isGraphViewShowing {
+            UIView.transitionFromView(graphView,
+                                      toView: counterView,
+                                      duration: 1.0,
+                                      options: [.TransitionFlipFromLeft,
+                                                .ShowHideTransitionViews],
+                                      completion: nil)
+        } else {
+            UIView.transitionFromView(counterView,
+                                      toView: graphView,
+                                      duration: 1.0,
+                                      options: [.TransitionFlipFromRight,
+                                                .ShowHideTransitionViews],
+                                      completion: nil)
+
+            setupGraphDisplay()
+        }
+        isGraphViewShowing = !isGraphViewShowing
     }
 }
