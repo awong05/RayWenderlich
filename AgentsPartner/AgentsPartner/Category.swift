f583718c3454dd497af5233c720107e177cbe2b6
Andy Wong <awong@propelmarketing.com>
2016-08-27 23:01:19 -0400

    Basic usage of Realm.

---
+++ AgentsPartner/AgentsPartner/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  Agents Partner
+//
+//  Created by Andy Wong on 8/27/16.
+//  Copyright © 2016 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+  dynamic var name = ""
+}
---
+++ AgentsPartner/README.md
@@ -0,0 +1,1 @@
+Source: https://www.raywenderlich.com/112544/realm-tutorial-getting-started
