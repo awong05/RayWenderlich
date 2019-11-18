2a04e33d30674d1b088b25b9b8dbd3b9a72ea555
Andy W <Andy.Kee.Wong@gmail.com>
2019-11-17 22:45:18 -0500

    Introduction to SwiftUI.

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Andy Wong on 11/25/19.
+//  Copyright © 2019 Andy Wong. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
